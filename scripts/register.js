18eedba159a7496d7b6751bea9edb5215d688798
shakelinz <63788535+shakelinz@users.noreply.github.com>
2025-04-22 14:20:12 +0300

    start of register logics

---
+++ scripts/register.js
@@ -0,0 +1,6 @@
+import { User, Task} from "./classes.js";
+let users = JSON.parse(localStorage.getItem("users"));
+// Function to insert the user details to the localstorage
+function register_handler(event) {
+    event.preventDefault();
+}
